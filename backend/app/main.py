46f263216ae31f6e6b643f3a74ced8f24da2fee4
lightbender62 <lightbender62@gmail.com>
2026-07-30 14:51:14 +0530

    minor changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://option-pricing-portal.vercel.app",
+        "https://option-pricing-portal.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
